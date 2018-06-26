5064ba5fa3af4d22853a08ca0967b282c7e35ece
黄蜀林 <1285653363@qq.com>
2018-06-26 15:18:56 +0800

    测试后台

--- application/admin/controller/Init.php
+++ application/admin/controller/Init.php
@@ -11,4 +11,22 @@ class Init extends Controller
         parent::_initialize();
         $this->is_login();
     }
+
+    /**
+     * 判断是否登录
+     * @author：xuewl01 <xuewl@xuewl.cn>
+     * @copyright: 雪毅网络官方团队
+     * @date：2016-07-12
+     * @version：1.0
+    */
+    final public function is_login() {  
+        if(request()->controller() == 'Index' && request()->action() == 'login') {
+            return true;
+        } else {
+            $userid = cookie('userid');
+            if(session('?userid') === FALSE || session('?roleid') === FALSE || $userid != session('userid')) {
+                $this->error('您尚未登录', url('Admin/Index/login'));
+            }
+        }
+    }
 }
