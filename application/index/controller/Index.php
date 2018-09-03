a42221c5b9cd0ccae45e4d08a7d3821a4ae44706
sking <452853270@qq.com>
2018-09-03 14:41:31 +0800

    合并

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,71 +1,62 @@
-<?php
-/**
- * 首页
- */
-namespace app\index\controller;
-use app\index\model\Ads;
-use app\index\model\Comments;
-use app\index\model\Contents;
-use think\captcha\Captcha;
-
-class Index extends Base
-{
-    /* ========== 初始化 ========== */
-    public function _initialize()
-    {
-        parent::_initialize();
-    }
-
-    /*======= 首页 ======*/
-    public function index()
-    {
-        /*------ 首页banner图 ------*/
-        $banner_info = Ads::field(['img_url'])->where(['type'=>1,'status'=>1])->select();
-        $this->assign('banner_info',$banner_info);
-
-        /*------ 文章信息 ------*/
-        //博主置顶
-        $content_top = Contents::field(['title','content'])->where(['is_top'=>1])->find();
-        $this->assign('content_top',$content_top);
-        //最新发布
-        $content_info = Contents::where(['is_top'=>0])
-            ->limit(10)
-            ->order(['release_time'=>'desc'])
-            ->select();
-        $this->assign('content_info',$content_info);
-
-        $ids = [];
-        foreach($content_info as $k=>$v){
-            if(in_array($k,[0,1,2,3,4,5])){
-                $ids[] = $v['id'];
-            }
-        }
-
-        Comments::field(['id','content_id'])->whereIn('content_id',$ids)->select();
-
-
-        /*------ 网站配置【站点公告-微信公众号】 ------*/
-        $this->assign('config_info',$this->config_info);
-
-        /*------ 热门标签 ------*/
-        $content_label = Contents::field(['label'])
-            ->order(['release_time'=>'desc'])
-            ->select();
-        $captcha = new Captcha();
-        $label_arr = [];
-        foreach($content_label as $k=>$v){
-            $label= explode(' ',$v['label']);
-            foreach ($label as $key=>$val){
-                $label_arr[] = $val;
-            }
-        }
-        $label_arr =  array_unique($label_arr);
-        $this->assign('label_arr',$label_arr);
-        /*------ 友情链接 ------*/
-        $href_info = Ads::field(['name','jump_url'])->where(['type'=>2,'status'=>1])->select();
-        $this->assign('href_info',$href_info);
-
-        $this->assign('action',request()->controller());
-        return view();
-    }
-}
+<?php
+/**
+ * 首页
+ */
+namespace app\index\controller;
+use app\index\model\Ads;
+use app\index\model\Comments;
+use app\index\model\Contents;
+
+class Index extends Base
+{
+    /* ========== 初始化 ========== */
+    public function _initialize()
+    {
+        parent::_initialize();
+    }
+
+    /*======= 首页 ======*/
+    public function index()
+    {
+        /*------ 首页banner图 ------*/
+        $banner_info = Ads::field(['img_url'])->where(['type'=>1,'status'=>1])->select();
+        $this->assign('banner_info',$banner_info);
+
+        /*------ 文章信息 ------*/
+        //博主置顶
+        $content_top = Contents::field(['title','content'])->where(['is_top'=>1])->find();
+        $this->assign('content_top',$content_top);
+        //最新发布
+        $content_info = Contents::withCount(['comments'])
+            ->where(['is_top'=>0])
+            ->limit(10)
+            ->order(['release_time'=>'desc'])
+            ->select();
+        $this->assign('content_info',$content_info);
+
+        /*------ 网站配置【站点公告-微信公众号】 ------*/
+        $this->assign('config_info',$this->config_info);
+
+        /*------ 热门标签 ------*/
+        $content_label = Contents::field(['label'])
+            ->order(['release_time'=>'desc'])
+            ->select();
+
+        $label_arr = [];
+        foreach($content_label as $k=>$v){
+            $label= explode(' ',$v['label']);
+            foreach ($label as $key=>$val){
+                $label_arr[] = $val;
+            }
+        }
+        $label_arr =  array_unique($label_arr);
+        $this->assign('label_arr',$label_arr);
+        /*------ 友情链接 ------*/
+        $href_info = Ads::field(['name','jump_url'])->where(['type'=>2,'status'=>1])->select();
+        $this->assign('href_info',$href_info);
+
+        //标识选中
+        $this->assign('action',request()->controller());
+        return view();
+    }
+}
--- application/index/controller/Resources.php
+++ application/index/controller/Resources.php
@@ -1,18 +1,34 @@
-<?php
-/**
- * 资源下载
- */
-
-namespace app\index\controller;
-use think\Controller;
-
-class Resources extends Controller
-{
-    /*======= 资源下载 ======*/
-    public function index()
-    {
-        
-		$this->assign('action',request()->controller());
-        return view();
-    }
+<?php
+/**
+ * 资源下载
+ */
+
+namespace app\index\controller;
+use app\index\model\Contents;
+use think\Controller;
+
+class Resources extends Controller
+{
+    /*======= 资源下载 ======*/
+    public function index()
+    {
+        //标识选中
+		$this->assign('action',request()->controller());
+
+        //最新发布
+        $content_title = Contents::field(['id','title'])
+            ->limit(10)
+            ->order(['release_time'=>'desc'])
+            ->select();
+        $this->assign('content_title',$content_title);
+
+        //资源信息
+        $content_info =  Contents::withCount(['comments'])
+                ->where('cover_img','neq','')
+                ->order(['release_time'=>'desc'])
+                ->paginate(2);
+        $this->assign('content_info',$content_info);
+
+        return view();
+    }
 }
